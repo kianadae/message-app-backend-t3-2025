bb382e40c97b449b4942339ca242e77f5ba6a2e7
20243212 <chad001@ess.ais.ac.nz>
2025-09-29 17:24:52 +1300

    1. created models 2. created serializers 3. created viewsets 4. add in route

---
+++ message/viewsets.py
@@ -0,0 +1,13 @@
+from message.serializers import Chat_roomSerializer, MessageSerializer
+from message.models import Chat_room, Message
+from rest_framework import viewsets
+
+
+class Chat_roomViewSet(viewsets.ModelViewSet):
+    queryset = Chat_room.objects.all()
+    serializer_class = Chat_roomSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
